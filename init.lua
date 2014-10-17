ceda8b6e78ec90163efb2d1bf3a2504d7739b846
Nicholas Leonard <nick@nikopia.org>
2014-10-16 21:05:10 -0400

    initial commit for torch.treemax

---
+++ init.lua
@@ -0,0 +1,3 @@
+require 'torch'
+
+torch.include('torchx', 'treemax.lua')
---
+++ treemax.lua
@@ -0,0 +1,32 @@
+local treeMaxBuffer = {}
+function torch.treemax(tensor, treeSize)
+   assert(torch.type(treeSize) == 'table')
+   assert(tensor:dim() == 1)
+   local tmb = treeMaxBuffer[torch.type(tensor)] -- upvalue
+   if not tmb then
+      tmb = {
+         mean = tensor.new(),
+         max = tensor.new(),
+         idx = torch.LongTensor()
+      }
+      
+      treeMaxBuffer[torch.type(tensor)] = tmb
+   end
+   
+   local lvl = tensor
+   local maxIdx, maxVal = 0, 0
+   for i=1,#treeSize do
+      lvl = lvl:view(treeSize[i], -1)
+      local lvlStride = lvl:size(2)
+      if i < #treeSize do
+         tmb.mean:mean(lvl, 2)
+      end
+      tmb.max:max(tmb.idx, tmb.mean:select(2,1), 1)
+      local lvlMax, lvlIdx = tmb.max[1], tmb.idx[1]
+      lvl = lvl[lvlIdx]
+      table.insert(maxPath, lvlIdx)
+      maxIdx = maxIdx + (lvlIdx-1)*lvlStride
+      maxVal = lvlMax
+   end
+   return maxVal, maxIdx
+end
